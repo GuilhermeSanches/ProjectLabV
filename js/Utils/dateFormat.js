e33ade92aa9fbff6c6f618f61b9785176184d7a1
JessicaAdriele <jessica-adriele2011@live.com>
2015-06-14 16:58:22 -0300

    Correção do script de formatação de data

--- js/Utils/dateFormat.js
+++ js/Utils/dateFormat.js
@@ -1,11 +1,11 @@
 var DateFormat = {
     format: function (date){
         split = date.split('-');
-        var date = new Date(split[0], split[1], split[2]); 
+        var date = new Date(split[0], split[1]-1, split[2]); 
         var day = date.getDate();
         if (day.toString().length == 1)
           day = "0"+day;
-        var month = date.getMonth();
+        var month = date.getMonth()+1;
         if (month.toString().length == 1)
           month = "0"+month;
         var year = date.getFullYear();  
