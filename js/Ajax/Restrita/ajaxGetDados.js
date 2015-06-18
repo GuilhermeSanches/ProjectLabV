bb1175de439f0b4306d4be39f6b9b892225f47d1
Guilherme Sanches Pereira <guilherme_1993pa@hotmail.com>
2015-06-18 16:15:27 -0300

    correção js

--- js/Ajax/Restrita/ajaxGetDados.js
+++ js/Ajax/Restrita/ajaxGetDados.js
@@ -35,7 +35,7 @@ var Resume_getDados = {
     
     getDados: function(dados){   
         var obj = new Array();
-        alert(dados);
+        
          obj = JSON.parse(dados);  
         
         
